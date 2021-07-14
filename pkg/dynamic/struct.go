8481d86229c1e896e69b4b44caac2b720b848b9d
B1AVK <QjFBVks@gmail.com>
2021-07-14 22:41:38 +0700

    implement dynamic struct

---
+++ pkg/dynamic/struct.go
@@ -0,0 +1,38 @@
+package dynamic
+
+import (
+	"reflect"
+
+	"github.com/b1avk/thrift/pkg/thrift"
+)
+
+type TStruct struct {
+	typ     reflect.Type
+	value   reflect.Value
+	encoder InternalEncoder
+}
+
+func NewTStruct(v reflect.Type) *TStruct {
+	mustBe(v, reflect.Struct)
+	return &TStruct{
+		typ:     v,
+		encoder: InternalEncoderOf(v),
+	}
+}
+
+func (e *TStruct) New() {
+	e.value.Set(reflect.New(e.typ).Elem())
+}
+
+func (e *TStruct) Write(p thrift.TProtocol) error {
+	return e.encoder.Encode(e.value, p)
+}
+
+func (e *TStruct) Read(p thrift.TProtocol) error {
+	e.New()
+	return e.encoder.Decode(e.value, p)
+}
+
+func (e *TStruct) Value() reflect.Value {
+	return e.value
+}
