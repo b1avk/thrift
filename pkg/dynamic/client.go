dfedf1d9988bbcdc8be2fe52bd6c2b2937dae7b1
B1AVK <QjFBVks@gmail.com>
2021-07-15 00:31:08 +0700

    implement dynamic service client

---
+++ pkg/dynamic/client.go
@@ -0,0 +1,43 @@
+package dynamic
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/b1avk/thrift/pkg/thrift"
+)
+
+func WrapServiceClient(s interface{}, c thrift.TClient) interface{} {
+	sv := reflect.ValueOf(s)
+	if sv.Kind() == reflect.Ptr {
+		sv = sv.Elem()
+	}
+	st := sv.Type()
+	if st.Kind() != reflect.Struct {
+		panic("dynamic.WrapServiceClient: service must be struct")
+	}
+	n := st.NumField()
+	for i := 0; i < n; i++ {
+		if m, ok := makeClientMethod(c, st.Field(i)); ok {
+			sv.Field(i).Set(m)
+		}
+	}
+	return s
+}
+
+func makeClientMethod(c thrift.TClient, v reflect.StructField) (reflect.Value, bool) {
+	f, err := parseDynamicField(v)
+	if err != nil {
+		return reflect.Value{}, false
+	}
+	ctx := context.Background()
+	return reflect.MakeFunc(v.Type, func(args []reflect.Value) (results []reflect.Value) {
+		if f.hasContext {
+			ctx = args[0].Interface().(context.Context)
+			args = args[1:]
+		}
+		res := f.NewResult()
+		err := c.Call(ctx, f.method, f.NewArgs(args), res)
+		return f.ReturnResult(res, err)
+	}), true
+}
---
+++ pkg/dynamic/service.go
@@ -0,0 +1,105 @@
+package dynamic
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+var contextType = reflect.TypeOf((*context.Context)(nil)).Elem()
+var errorType = reflect.TypeOf((*error)(nil)).Elem()
+
+type dynamicField struct {
+	splited []string
+
+	method       string
+	hasContext   bool
+	returnError  bool
+	args, result *TStruct
+}
+
+func parseDynamicField(t reflect.StructField) (f dynamicField, err error) {
+	splited := strings.Split(t.Tag.Get("thrift"), " ")
+	f.method = splited[0]
+	f.splited = splited[1:]
+	ft := t.Type
+	si := []reflect.StructField{}
+	ni := ft.NumIn()
+	var id int
+	for i := 0; i < ni; i++ {
+		ti := ft.In(i)
+		if i == 0 || ti.AssignableTo(contextType) {
+			f.hasContext = true
+			continue
+		}
+		if id, err = f.nextIdentity(); err != nil {
+			return
+		}
+		si = append(si, reflect.StructField{
+			Name: "F" + strconv.Itoa(i),
+			Type: ti,
+			Tag:  reflect.StructTag(fmt.Sprintf(`thrift:"%v"`, id)),
+		})
+	}
+	fmt.Println(si)
+	if len(si) != 0 {
+		f.args = NewTStruct(reflect.StructOf(si))
+	}
+	no := ft.NumOut()
+	if no != 0 {
+		so := []reflect.StructField{}
+		for i := 0; i < no; i++ {
+			to := ft.Out(i)
+			if i == no || to.AssignableTo(errorType) {
+				f.returnError = true
+				continue
+			}
+			if id, err = f.nextIdentity(); err != nil {
+				return
+			}
+			so = append(so, reflect.StructField{
+				Name: "F" + strconv.Itoa(i),
+				Type: to,
+				Tag:  reflect.StructTag(fmt.Sprintf(`thrift:"%v"`, id)),
+			})
+		}
+		if len(so) != 0 {
+			f.result = NewTStruct(reflect.StructOf(so))
+		}
+	}
+	return
+}
+
+func (f dynamicField) NewArgs(args []reflect.Value) *TStruct {
+	v := f.args.Copy()
+	v.New()
+	return v
+}
+
+func (f dynamicField) NewResult() *TStruct {
+	return f.result.Copy()
+}
+
+func (f dynamicField) ReturnResult(res *TStruct, err error) (results []reflect.Value) {
+	if res != nil {
+		for i := range res.encoder.fieldEncoderByIndex {
+			results = append(results, res.value.Field(i))
+		}
+	}
+	if f.returnError {
+		results = append(results, reflect.ValueOf(err))
+	}
+	return
+}
+
+func (f *dynamicField) nextIdentity() (v int, err error) {
+	if len(f.splited) == 0 {
+		err = fmt.Errorf("no splited left")
+	} else {
+		v, err = strconv.Atoi(f.splited[0])
+		f.splited = f.splited[1:]
+	}
+	return
+}
