4343a5a14f4a2f8e7b469288a19008509fde04a2
B1AVK <QjFBVks@gmail.com>
2021-07-14 22:22:29 +0700

    implement Write/Read method

--- pkg/thrift/exception_application.go
+++ pkg/thrift/exception_application.go
@@ -1,6 +1,6 @@
 package thrift
 
-type TApplicationError byte
+type TApplicationError = int32
 
 const (
 	TApplicationErrorUnknown TApplicationError = iota
@@ -23,4 +23,81 @@ type TApplicationException struct {
 
 func (e *TApplicationException) Error() string {
 	return e.Message
+}
+
+func (e *TApplicationException) Write(p TProtocol) (err error) {
+	if err = p.WriteStructBegin(TStructHeader{
+		Name: "TApplicationException",
+	}); err != nil {
+		return
+	}
+	if len(e.Message) > 0 {
+		if err = p.WriteFieldBegin(TFieldHeader{"message", STRING, 1}); err != nil {
+			return
+		}
+		if err = p.WriteString(e.Message); err != nil {
+			return
+		}
+		if err = p.WriteFieldEnd(); err != nil {
+			return
+		}
+	}
+	if err = p.WriteFieldBegin(TFieldHeader{"type", I32, 2}); err != nil {
+		return
+	}
+	if err = p.WriteI32(e.Type); err != nil {
+		return
+	}
+	if err = p.WriteFieldEnd(); err != nil {
+		return
+	}
+	if err = p.WriteFieldStop(); err != nil {
+		return
+	}
+	err = p.WriteStructEnd()
+	return
+}
+
+func (e *TApplicationException) Read(p TProtocol) (err error) {
+	if _, err = p.ReadStructBegin(); err != nil {
+		return
+	}
+	var h TFieldHeader
+	for {
+		if h, err = p.ReadFieldBegin(); err != nil {
+			return
+		}
+		if h.Type == STOP {
+			break
+		}
+		switch h.Identity {
+		case 1:
+			if h.Type == STRING {
+				e.Message, err = p.ReadString()
+			} else {
+				err = p.Skip(h.Type)
+			}
+			if err != nil {
+				return
+			}
+		case 2:
+			if h.Type == I32 {
+				e.Type, err = p.ReadI32()
+			} else {
+				err = p.Skip(h.Type)
+			}
+			if err != nil {
+				return
+			}
+		default:
+			if err = p.Skip(h.Type); err != nil {
+				return
+			}
+		}
+		if err = p.ReadFieldEnd(); err != nil {
+			return
+		}
+	}
+	err = p.ReadStructEnd()
+	return
 }
